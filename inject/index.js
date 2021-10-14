09d455ce34a9ee0e16db30e1f64ef15d201de109
liangqi <liliangqi@shuidi-inc.com>
2021-10-14 16:06:31 +0800

    improve: 优化过渡动画

--- inject/index.js
+++ inject/index.js
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request.info)
   if(request.info === 'changeMode') {
     let root = document.getElementsByTagName('html')[0];
-    if(!root.classList.contains("dark")){
-      root.classList.add("dark");
+    if(!root.classList.contains("dimmer-dark")){
+      root.classList.add("dimmer-dark");
     }else{
-      root.classList.remove('dark');
+      root.classList.remove('dimmer-dark');
     }
   }
   if(request.info === 'getMode') {
     let root = document.getElementsByTagName('html')[0];
-    if(!root.classList.contains("dark")){
+    if(!root.classList.contains("dimmer-dark")){
       sendResponse({
         has: false
       })
