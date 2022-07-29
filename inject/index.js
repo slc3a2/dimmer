0b477fc0b1fb1bb205a9cb8eebd0b7e0bb0548a0
liangqi <liliangqi@yinxiang.com>
2022-07-29 15:31:33 +0800

    release 1.0.5

--- inject/index.js
+++ inject/index.js
@@ -1,42 +1,41 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if(request.info === 'changeMode') {
-    let root = document.getElementsByTagName('html')[0];
-    if(!root.classList.contains("dimmer-dark")){
+  if (request.info === "changeMode") {
+    const root = document.getElementsByTagName("html")[0];
+    if (!root.classList.contains("dimmer-dark")) {
       root.classList.add("dimmer-dark");
-      sessionStorage.setItem("ohmydimmer-isDark", "true")
-    }else{
-      root.classList.remove('dimmer-dark');
-      sessionStorage.setItem("ohmydimmer-isDark", "false")
+      sessionStorage.setItem("ohmydimmer-isDark", "true");
+    } else {
+      root.classList.remove("dimmer-dark");
+      sessionStorage.setItem("ohmydimmer-isDark", "false");
     }
   }
-  if(request.info === 'getMode') {
-    let root = document.getElementsByTagName('html')[0];
-    if(!root.classList.contains("dimmer-dark")){
+  if (request.info === "getMode") {
+    const root = document.getElementsByTagName("html")[0];
+    if (!root.classList.contains("dimmer-dark")) {
       sendResponse({
-        has: false
-      })
-    }else{
+        has: false,
+      });
+    } else {
       sendResponse({
-        has: true
-      })
+        has: true,
+      });
     }
   }
-})
+});
 
 function main() {
-  if(sessionStorage.getItem("ohmydimmer-flag")){
-    // 页面被刷新
-    let t = sessionStorage.getItem("ohmydimmer-isDark")
-    let root = document.getElementsByTagName('html')[0]
-    if(t === "true") {
-      root.classList.add("dimmer-dark")
-    }else {
-      root.classList.remove('dimmer-dark');
+  if (sessionStorage.getItem("ohmydimmer-flag")) {
+    const t = sessionStorage.getItem("ohmydimmer-isDark");
+    const root = document.getElementsByTagName("html")[0];
+    if (t === "true") {
+      root.classList.add("dimmer-dark");
+    } else {
+      root.classList.remove("dimmer-dark");
     }
-  }else{
+  } else {
     // 首次被加载
-    sessionStorage.setItem("ohmydimmer-flag", "true")
+    sessionStorage.setItem("ohmydimmer-flag", "true");
   }
 }
 
-main()
+main();
--- popup/index.js
+++ popup/index.js
@@ -1,48 +1,55 @@
-document.querySelector('.normal').onclick = function(e) {
-  if(this.classList.contains('cur')){
-    this.classList.remove('cur')
-    changeModeHandle()
-  }else{
-    this.classList.add('cur')
-    changeModeHandle()
+document.querySelector(".normal").onclick = function (e) {
+  if (this.classList.contains("cur")) {
+    this.classList.remove("cur");
+    changeModeHandle();
+  } else {
+    this.classList.add("cur");
+    changeModeHandle();
   }
-}
+};
 
 function changeModeHandle() {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, (tabs) => {
-    let message = {
-        info: 'changeMode'
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      let message = {
+        info: "changeMode",
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message, (res) => {});
     }
-    chrome.tabs.sendMessage(tabs[0].id, message, res => {
-    })
-  })
+  );
 }
-window.onload = function(){
+window.onload = function () {
   const version = chrome.runtime.getManifest().version;
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, (tabs) => {
-    let message = {
-        info: 'getMode'
-    }
-    chrome.tabs.sendMessage(tabs[0].id, message, res => {
-        if(res.has){
-          document.querySelector('.normal').classList.add('cur')
-        }else{
-          document.querySelector('.normal').classList.remove('cur')
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      let message = {
+        info: "getMode",
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message, (res) => {
+        if (res.has) {
+          document.querySelector(".normal").classList.add("cur");
+        } else {
+          document.querySelector(".normal").classList.remove("cur");
         }
-    })
-  })
-  
-  document.querySelector('.gh').onclick = function(e) {
-    window.open('https://github.com/codedance98/oh-my-dimmer')
-  }
-  
-  document.querySelector('.score').onclick = function(e) {
-    window.open('https://chrome.google.com/webstore/detail/%E5%85%B3%E7%81%AF/dnidbhhpcjgffjophhebfelbcnonoclh?hl=zh-CN')
-  }
-}
+      });
+    }
+  );
+
+  document.querySelector(".gh").onclick = function (e) {
+    window.open("https://github.com/slc3a2/dimmer");
+  };
+
+  document.querySelector(".score").onclick = function (e) {
+    window.open(
+      "https://chrome.google.com/webstore/detail/%E5%85%B3%E7%81%AF/dnidbhhpcjgffjophhebfelbcnonoclh?hl=zh-CN"
+    );
+  };
+};
