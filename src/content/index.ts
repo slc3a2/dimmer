35a38a8d8c8a213a82f480c21a959fb0d32e5cc0
slc3a2 <slc3a2@gmail.com>
2023-05-15 10:46:33 +0800

    chore: select snap part

--- src/content/index.ts
+++ src/content/index.ts
@@ -35,7 +35,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         cursor: crosshair;
         display: none;
         backgorund-color: rgba(0,0,0,0.2);
-        border: 1px solid #fff;
+        border: 1px solid rgba(0,0,0,0.3);
+        z-index: 1002;
         `
 
     console.log(select)
@@ -48,15 +49,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       console.log(e)
       e.stopPropagation()
       e.preventDefault()
-
+    
       select.style.display = 'block'
       select.style.left = `${e.clientX}px`
       select.style.top = `${e.clientY}px`
-
-      startX = e.clientX
-      startY = e.clientY
+    
+      startX = e.clientX - dom.offsetLeft
+      startY = e.clientY - dom.offsetTop
     }
-
+    
     dom.onmouseup = (e) => {
       select.style.display = 'none'
       var croppedCanvas = document.createElement('canvas')
@@ -64,31 +65,38 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       if (!croppedContext) {
         return
       }
-      const width = Number(select.style.width.replace('px', ''))
-      const height = Number(select.style.height.replace('px', ''))
+      const width = Math.abs(e.clientX - startX - dom.offsetLeft)
+      const height = Math.abs(e.clientY - startY - dom.offsetTop)
       croppedCanvas.width = width
       croppedCanvas.height = height
-
+    
       const img = document.querySelector('colora img') as CanvasImageSource
-      console.log(img, startX, startY, width, height)
+
+      console.log(img, startX, startY, width, height, 0, 0, width, height)
       croppedContext.drawImage(img, startX, startY, width, height, 0, 0, width, height)
-
       const croppedDataURL = croppedCanvas.toDataURL()
-      window.open(croppedDataURL)
-    }
+      console.log(croppedDataURL)
 
+      debugBase64(croppedDataURL)
+    }
+    
     dom.onmousemove = (e) => {
       e.stopPropagation()
       e.preventDefault()
-      let width = e.clientX - startX
-      let height = e.clientY - startY
-      select.style.width = `${width}px`
-      select.style.height = `${height}px`
+      let width = e.clientX - startX - dom.offsetLeft
+      let height = e.clientY - startY - dom.offsetTop
+      select.style.width = `${Math.abs(width)}px`
+      select.style.height = `${Math.abs(height)}px`
     }
 
     dom.oncontextmenu = (e) => {
       e.preventDefault()
       select.style.display = 'none'
+    }
+
+    function debugBase64(base64URL: string){
+      const win = window.open();
+      win?.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
     }
   }
   sendResponse('get the message')
